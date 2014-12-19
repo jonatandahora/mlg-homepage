6772fdcf3d29849c1f8b78db1485ccd3b8554057
Jonatan da Hora <jonatandahora@gmail.com>
2014-12-19 15:14:59 -0200

    loader

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Homepage::Application.routes.draw do
   resources :tutoriais
 
   get "home/index"
-  get "public/loaderio-fc5d807f38a2814c352cfeb5d1aed337.txt"
   resources :contacts
 
   match '/contato',     to: 'contacts#new',             via: 'get'
