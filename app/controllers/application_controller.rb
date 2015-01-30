5ec36b5e5de827b47a3465d242134870aefb1bcf
Jonatan da Hora <jonatandahora@gmail.com>
2015-01-30 16:57:58 -0200

    disable chat - redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :check_url
 
+
+
+  def check_url
+    current = root_url(:only_path => false)
+    home = 'http://home.mlgbrasil.tv/'
+    if(current!= home)
+    redirect_to home
+    end
+  end
 end
